2be53f19a566570f03388ab5ef5383c3cc7dfce4
skrtdev <64538010+skrtdev@users.noreply.github.com>
2021-12-02 17:39:58 +0100

    Add hex_only parameter to get_pending_transactions_limit

--- denaro/database.py
+++ denaro/database.py
@@ -74,11 +74,12 @@ class Database:
             await connection.execute('DELETE FROM blocks WHERE id = $1', id)
             await connection.execute(f'ALTER SEQUENCE blocks_id_seq RESTART {await self.get_next_block_id()}')
 
-    async def get_pending_transactions_limit(self, limit: int = 1000) -> List[Transaction]:
+    async def get_pending_transactions_limit(self, limit: int = 1000, hex_only: bool = False) -> List[Transaction]:
         async with self.pool.acquire() as connection:
             txs = await connection.fetch(f'SELECT tx_hex FROM pending_transactions ORDER BY fees DESC LIMIT {limit}')
+            if hex_only:
+                return [tx['tx_hex'] for tx in txs]
             return [await Transaction.from_hex(tx['tx_hex']) for tx in txs]
-            return [tx for tx in [await Transaction.from_hex(tx['tx_hex']) for tx in await stmt.fetch()] if await tx.verify()]
 
     async def add_transaction(self, transaction: Union[Transaction, CoinbaseTransaction], block_hash: str):
         tx_hex = transaction.hex()
