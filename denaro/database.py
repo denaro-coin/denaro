93b58f8e13b9f79d59f469d6c090961ef6b1638f
skrtdev <64538010+skrtdev@users.noreply.github.com>
2021-12-30 13:23:19 +0100

    Add database.get_transactions() and database.get_transaction_by_contains_multi()

--- denaro/database.py
+++ denaro/database.py
@@ -130,10 +130,19 @@ class Database:
             res = await connection.fetchrow('SELECT tx_hex FROM pending_transactions WHERE tx_hash = $1', tx_hash)
         return await Transaction.from_hex(res['tx_hex'], check_signatures) if res is not None else None
 
-    async def get_transactions_by_contains(self, contains: str):
+    async def get_transactions(self, tx_hashes: List[str]):
         async with self.pool.acquire() as connection:
-            res = await connection.fetch('SELECT tx_hex FROM transactions WHERE tx_hex LIKE $1 AND tx_hash != $2', f"%{contains}%", contains)
-        return [await Transaction.from_hex(res['tx_hex']) for res in res] if res is not None else None
+            res = await connection.fetch('SELECT tx_hex FROM transactions WHERE tx_hash = ANY($1)', tx_hashes)
+        return {sha256(res['tx_hex']): await Transaction.from_hex(res['tx_hex']) for res in res}
+
+    async def get_transaction_by_contains_multi(self, contains: List[str], ignore: str):
+        async with self.pool.acquire() as connection:
+            res = await connection.fetchrow(
+                'SELECT tx_hex FROM transactions WHERE tx_hex LIKE ANY($1) AND tx_hash != $2',
+                [f"%{contains}%" for contains in contains],
+                ignore
+            )
+        return await Transaction.from_hex(res['tx_hex']) if res is not None else None
 
     async def get_pending_transactions_by_contains(self, contains: str):
         async with self.pool.acquire() as connection:
