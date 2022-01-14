cc861e096dedcf236d51802c0862b4b782aaf4b1
skrtdev <64538010+skrtdev@users.noreply.github.com>
2022-01-14 18:12:00 +0100

    Move create_transaction() into wallet.utils

---
+++ denaro/wallet/utils.py
@@ -0,0 +1,44 @@
+from decimal import Decimal
+
+from fastecdsa import keys
+
+from denaro import Database
+from denaro.constants import CURVE
+from denaro.helpers import point_to_string
+from denaro.transactions import Transaction, TransactionOutput
+
+
+async def create_transaction(private_keys, receiving_address, amount, message: bytes = None):
+    denaro_database: Database = await Database.get()
+    amount = Decimal(amount)
+    inputs = []
+    for private_key in private_keys:
+        address = point_to_string(keys.get_public_key(private_key, CURVE))
+        inputs.extend(await denaro_database.get_spendable_outputs(address, check_pending_txs=True))
+        if sum(input.amount for input in inputs) >= amount:
+            break
+    if not inputs:
+        raise Exception('No spendable outputs')
+
+    if sum(input.amount for input in inputs) < amount:
+        raise Exception(f"Error: You don\'t have enough funds")
+
+    most_amount = sorted(inputs, key=lambda item: item.amount, reverse=True)
+
+    transaction_inputs = []
+
+    for i, tx_input in enumerate(most_amount):
+        transaction_inputs.append(tx_input)
+        transaction_amount = sum(input.amount for input in transaction_inputs)
+        if transaction_amount >= amount:
+            break
+
+    transaction_amount = sum(input.amount for input in transaction_inputs)
+
+    transaction = Transaction(transaction_inputs, [TransactionOutput(receiving_address, amount=amount)], message)
+    if transaction_amount > amount:
+        transaction.outputs.append(TransactionOutput(address, transaction_amount - amount))
+
+    transaction.sign(private_keys)
+
+    return transaction
--- denaro/wallet/wallet.py
+++ denaro/wallet/wallet.py
@@ -2,11 +2,12 @@ import argparse
 import asyncio
 import os
 import sys
-from decimal import Decimal
 
 import pickledb
 import requests
 from fastecdsa import keys, curve
+
+from denaro.wallet.utils import create_transaction
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 sys.path.insert(0, dir_path + "/../..")
@@ -21,44 +22,6 @@ Database.credentials = {
     'user': os.environ.get('DENARO_DATABASE_USER', 'denaro'),
     'password': os.environ.get('DENARO_DATABASE_PASSWORD', '')
 }
-
-
-async def create_transaction(private_keys, receiving_address, amount):
-    denaro_database: Database = await Database.get()
-    amount = Decimal(amount)
-    inputs = []
-    for private_key in private_keys:
-        address = point_to_string(keys.get_public_key(private_key, curve.P256))
-        inputs.extend(await denaro_database.get_spendable_outputs(address, check_pending_txs=True))
-        if sum(input.amount for input in inputs) >= amount:
-            break
-    if not inputs:
-        raise Exception('No spendable outputs')
-
-    if sum(input.amount for input in inputs) < amount:
-        raise Exception(f"Error: You don\'t have enough funds")
-
-    most_amount = sorted(inputs, key=lambda item: item.amount, reverse=True)
-
-    transaction_inputs = []
-
-    for i, tx_input in enumerate(most_amount):
-        transaction_inputs.append(tx_input)
-        transaction_amount = sum(input.amount for input in transaction_inputs)
-        if transaction_amount >= amount:
-            break
-
-    transaction_amount = sum(input.amount for input in transaction_inputs)
-
-    transaction = Transaction(transaction_inputs, [TransactionOutput(receiving_address, amount=amount)])
-    if transaction_amount > amount:
-        transaction.outputs.append(TransactionOutput(address, transaction_amount - amount))
-
-    transaction.sign(private_keys)
-
-    await denaro_database.add_pending_transaction(transaction)
-    requests.get('https://denaro-node.gaetano.eu.org/push_tx', {'tx_hex': transaction.hex()}, timeout=10)
-    return transaction
 
 
 async def main():
@@ -103,6 +66,8 @@ async def main():
         amount = args.amount
 
         tx = await create_transaction(db.get('private_keys'), receiver, amount)
+        await denaro_database.add_pending_transaction(tx)
+        requests.get('https://denaro-node.gaetano.eu.org/push_tx', {'tx_hex': tx.hex()}, timeout=10)
         print(f'Transaction pushed. Transaction hash: {sha256(tx.hex())}')
 
 
