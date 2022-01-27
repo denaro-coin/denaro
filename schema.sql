f46f28066644dc29453b93de8b986fa35d6d0c05
skrtdev <64538010+skrtdev@users.noreply.github.com>
2022-01-27 17:51:31 +0100

    Fix schema.sql

--- schema.sql
+++ schema.sql
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS blocks (
 CREATE TABLE IF NOT EXISTS transactions (
 	block_hash CHAR(64) NOT NULL REFERENCES blocks(hash) ON DELETE CASCADE,
 	tx_hash CHAR(64) UNIQUE,
-	tx_hex TEXT UNIQUE,
+	tx_hex TEXT,
 	inputs_addresses TEXT[],
 	fees NUMERIC(14, 6) NOT NULL
 );
@@ -28,7 +28,7 @@ CREATE TABLE IF NOT EXISTS unspent_outputs (
 
 CREATE TABLE IF NOT EXISTS pending_transactions (
 	tx_hash CHAR(64) UNIQUE,
-	tx_hex VARCHAR(2048) UNIQUE,
+	tx_hex TEXT,
 	inputs_addresses TEXT[],
 	fees NUMERIC(14, 6) NOT NULL
 );
