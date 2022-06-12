c0abcc505e541fd1b755f48201770ca71e5613c7
skrtdev <64538010+skrtdev@users.noreply.github.com>
2022-06-12 20:21:25 +0200

    Fix miner

--- miner.py
+++ miner.py
@@ -1,54 +1,110 @@
-import asyncio
+import hashlib
 import sys
 import time
-import denaro
+from math import ceil
+from multiprocessing import Process
+
+import requests
+
 from denaro.constants import ENDIAN
-from denaro.manager import get_difficulty, check_block_is_valid, Manager, get_transactions_merkle_tree
-from denaro.helpers import sha256, timestamp
+from denaro.helpers import string_to_bytes, timestamp
+from denaro.manager import get_transactions_merkle_tree
 
-from icecream import ic
-
-from denaro.node.main import sync_blockchain, push_block, startup
-
-_print = print
-print = ic
+NODE = sys.argv[3].strip('/')+'/' if len(sys.argv) >= 4 else 'http://localhost:3006/'
 
 
-async def run():
-    await startup()
-    db = denaro.node.main.db
+def run(start: int = 0, step: int = 1, res: dict = None):
+    difficulty = res['difficulty']
+    decimal = difficulty % 1
+    last_block = res['last_block']
+    last_block['hash'] = last_block['hash'] if 'hash' in last_block else (30_06_2005).to_bytes(32, ENDIAN).hex()
+    last_block['id'] = last_block['id'] if 'id' in last_block else 0
+    chunk = last_block['hash'][-int(difficulty):]
 
+    charset = '0123456789abcdef'
+    if decimal > 0:
+        count = ceil(16 * (1 - decimal))
+        charset = charset[:count]
+        idifficulty = int(difficulty)
+
+        def check_block_is_valid(block_content: bytes) -> bool:
+            block_hash = hashlib.sha256(block_content).hexdigest()
+            return block_hash.startswith(chunk) and block_hash[idifficulty] in charset
+    else:
+        def check_block_is_valid(block_content: bytes) -> bool:
+            return hashlib.sha256(block_content).hexdigest().startswith(chunk)
+
+    address = sys.argv[1]
+    address_bytes = string_to_bytes(address)
+    t = time.time()
+    i = start
+    a = timestamp()
+    txs = res['pending_transactions_hashes']
+    merkle_tree = get_transactions_merkle_tree(txs)
+    assert all(len(tx) == 64 for tx in txs)
+    if start == 0:
+        print(f'difficulty: {difficulty}')
+        print(f'block number: {last_block["id"]}')
+        print(f'Confirming {len(txs)} transactions')
+    prefix = bytes.fromhex(last_block['hash']) + address_bytes + bytes.fromhex(merkle_tree) + a.to_bytes(4, byteorder=ENDIAN) + int(difficulty * 10).to_bytes(2, ENDIAN)
+    if len(address_bytes) == 33:
+        prefix = (2).to_bytes(1, ENDIAN) + prefix
     while True:
-        await sync_blockchain()
-        difficulty, last_block = await get_difficulty()
-        last_block['hash'] = last_block['hash'] if 'hash' in last_block else (30_06_2005).to_bytes(32, ENDIAN).hex()
-        print(difficulty)
-        Manager.difficulty = None
-        address = sys.argv[1]
-        t = time.process_time()
-        i = 0
-        a = timestamp()
-        txs = await db.get_pending_transactions_limit(1000)
-        merkle_tree = get_transactions_merkle_tree(txs)
-        prefix = bytes.fromhex(last_block['hash']) + bytes.fromhex(address) + bytes.fromhex(merkle_tree) + a.to_bytes(4, byteorder=ENDIAN) + int(difficulty * 10).to_bytes(2, ENDIAN)
-
         found = True
-        while not await check_block_is_valid(_hex := prefix + i.to_bytes(4, ENDIAN), (difficulty, last_block)):
-            i += 1
-            if i % 1000000 == 0:
-                elapsed_time = time.process_time() - t
-                _print(str(i / elapsed_time) + ' hash/s')
-                print(i)
-                if elapsed_time > 150:
+        check = 5000000 * step
+        while not check_block_is_valid(_hex := prefix + i.to_bytes(4, ENDIAN)):
+            if ((i := i + step) - start) % check == 0:
+                elapsed_time = time.time() - t
+                print(f'Worker {start + 1}: ' + str(int(i / step / elapsed_time / 1000)) + 'k hash/s')
+                if elapsed_time > 90:
                     found = False
                     break
         if found:
-            await sync_blockchain()
-            res = await push_block(None, _hex.hex(), [tx.hex() for tx in txs], False)
-            Manager.difficulty = None
-            if res['ok'] and txs:
-                for tx in txs:
-                    await db.remove_pending_transaction(sha256(tx.hex()))
+            print(_hex.hex())
+            print(','.join(txs))
+            r = requests.post(NODE + 'push_block', json={
+                'block_content': _hex.hex(),
+                'txs': txs,
+            }, timeout=20 + int((len(txs) or 1) / 3))
+            print(res := r.json())
+            if res['ok']:
+                print('BLOCK MINED\n\n')
+            exit()
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(run())
+
+def worker(start: int, step: int, res: dict):
+    while True:
+        try:
+            run(start, step, res)
+        except Exception:
+            raise
+            time.sleep(3)
+
+
+if __name__ == '__main__':
+    workers = int(sys.argv[2]) if len(sys.argv) >= 3 else 1
+    while True:
+        print(f'Starting {workers} workers')
+        res = None
+        while res is None:
+            try:
+                r = requests.get(NODE + 'get_mining_info', timeout=5)
+                res = r.json()['result']
+            except Exception as e:
+                print(e)
+                time.sleep(1)
+                pass
+        processes = []
+        for i in range(1, workers + 1):
+            print(f'Starting worker n.{i}')
+            p = Process(target=worker, daemon=True, args=(i-1, workers, res))
+            p.start()
+            processes.append(p)
+        elapsed_seconds = 0
+        while all(p.is_alive() for p in processes):
+            time.sleep(1)
+            elapsed_seconds += 1
+            if elapsed_seconds > 100:
+                break
+        for p in processes:
+            p.kill()
